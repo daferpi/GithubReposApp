70feaaa78f61aa172ab971a08d85ac1113f29e1c
daferpi <daferpi@protonmail.ch>
2018-08-17 16:07:36 +0200

    add mockup for repository in interactor

--- GithubReposAppTests/GithubInteractorTests.swift
+++ GithubReposAppTests/GithubInteractorTests.swift
@@ -9,6 +9,13 @@ import Nimble
 
 @testable import GithubReposApp
 
+enum ResposeResult {
+    case success
+    case empty
+    case error
+}
+
+
 class GithubInteractorTests:QuickSpec {
     
     override func spec() {
@@ -16,7 +23,7 @@ class GithubInteractorTests:QuickSpec {
         
         describe("test interactor") {
             beforeEach {
-                interactor = GithubInteractorImpl()
+                interactor = GithubInteractorImpl(repository: MockupRepo(responseResult: .success))
             }
             
             context("when interactor is create") {
@@ -26,5 +33,52 @@ class GithubInteractorTests:QuickSpec {
             }
         }
         
+    }
+}
+
+class MockupRepo: GithubRepository {
+    private let result:ResposeResult
+    
+    init(responseResult:ResposeResult) {
+        self.result = responseResult
+    }
+    
+    func listRepositories(completionHandler: @escaping CompletionHandlerList ) {
+        switch result {
+        case .success:
+            let repositories = self.createRepositories()
+            completionHandler(repositories, nil)
+        case .error:
+            completionHandler(nil, ApiError.invalidCall(message: "error"))
+        case .empty:
+            completionHandler([], nil)
+        }
+        
+    }
+    func detailRepository(owner:String, repoName:String, completionHandler: @escaping CompletionHandlerUnit) {
+        switch result {
+        case .success:
+            let repository = self.createRepository(index: 1)
+            completionHandler(repository, nil)
+        case .error:
+            completionHandler(nil, ApiError.invalidCall(message: "error"))
+        case .empty:
+            completionHandler(nil, nil)
+        }
+    }
+    
+    private func createRepositories() -> [Repository] {
+        
+        let repository1 = self.createRepository(index: 1)
+        
+        let repository2 = self.createRepository(index: 2)
+        
+        return [repository1, repository2]
+    }
+    
+    private func createRepository(index:Int) -> Repository {
+        let repositoryOwner = RepositoryOwner(login: "owner\(index)", id: index, avatar_url: "http://avatarUrl\(index).com", url: "http://Url\(index).com")
+        let repository = Repository(id: index, name: "repository\(index)", full_name: "repositoryFullName\(index)", owner: repositoryOwner, url: "http://url\(index)", description: "description\(index)")
+        return repository
     }
 }
